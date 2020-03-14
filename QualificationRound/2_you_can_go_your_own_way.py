23249dde333ece65703090e5c77cf1d85af74e17
sreeragh <sreeraghar@gmail.com>
2020-03-14 20:11:10 +0530

    code refactored

--- QualificationRound/2_you_can_go_your_own_way.py
+++ QualificationRound/2_you_can_go_your_own_way.py
@@ -2,26 +2,18 @@
 # https://codingcompetitions.withgoogle.com/codejam/round/0000000000051705/00000000000881da
 
 n_cases = int(input())
-sizes = []
 lydia_paths = []
 
 
-def get_new_path(size, explored_path):
-    steps_in_a_dir = size - 1
-    total_steps = steps_in_a_dir * 2
-    total_steps = len(explored_path)
-    new_path = ''
-    for i in range(total_steps):
-        if explored_path[i] == 'S':
-            new_path += 'E'
-        else:
-            new_path += 'S'
+def get_new_path(explored_path):
+    new_path = explored_path.replace(
+        'S', 'X').replace('E', 'S').replace('X', 'E')
     return new_path
 
 
 for i in range(n_cases):
-    sizes.append(int(input()))
+    input()  # size is not used
     lydia_paths.append(input())
 
 for i in range(n_cases):
-    print('Case #{}: {}'.format(i+1, get_new_path(sizes[i], lydia_paths[i])))
+    print('Case #{}: {}'.format(i+1, get_new_path(lydia_paths[i])))
