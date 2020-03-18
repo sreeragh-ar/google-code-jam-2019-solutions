cbeb0ba19d575cec106095cd72b35a20a1061589
sreeragh <sreeraghar@gmail.com>
2020-03-18 17:35:05 +0530

    code factorizing code refactores

--- QualificationRound/3_crypto_pangrams.py
+++ QualificationRound/3_crypto_pangrams.py
@@ -17,17 +17,39 @@ def get_all_prime_numbers(n):
     return prime_nums
 
 
+def factorise(keys, keys_product):
+    for prime in keys:
+        quotient, remainder = divmod(keys_product, prime)
+        if remainder == 0:
+            return prime, quotient
+
+
 def get_keys(possible_primes, encrypted_text):
-    encrypted_text = list(map(int, encrypted_text.split()))
+    # encrypted_text = list(map(int, encrypted_text.split()))
     keys = []
     for index, crypt in enumerate(encrypted_text):
-        for prime in possible_primes:
+        # for prime in possible_primes:
+        #     quotient, remainder = divmod(crypt, prime)
+        #     if remainder == 0:
+        #         keys.append(prime)
+        #         keys.append(quotient)
+        #         break
+        keys += factorise(possible_primes, crypt)
+    return list(set(keys))
+
+
+def decrypt_text(encrypted_text, cipher_keys):
+    text_len = len(encrypted_text)
+    decrypted = []
+    for i in range(text_len - 1):
+        candidate_chars = []
+        next_candidate_chars = []
+        for index, prime in enumerate(cipher_keys):
             quotient, remainder = divmod(crypt, prime)
             if remainder == 0:
-                keys.append(prime)
-                keys.append(quotient)
+                candidate_chars.append(prime)
+                candidate_chars.append(quotient)
                 break
-    return list(set(keys))
 
 
 n_cases = int(input())
@@ -38,7 +60,9 @@ for i in range(n_cases):
     lim_and_length = input().split()
     limits.append(int(lim_and_length[0]))
     lengths.append(int(lim_and_length[1]))
-    encrypted_texts.append(input())
+    encrypted_text = input()
+    encrypted_text = list(map(int, encrypted_text.split()))
+    encrypted_texts.append(encrypted_text)
 
 all_primes = get_all_prime_numbers(max(limits))
 # print(all_primes)
@@ -50,3 +74,4 @@ for i in range(n_cases):
     possible_primes = all_primes[:index_of_n]
     cipher_keys = get_keys(possible_primes, encrypted_texts[i])
     print(cipher_keys)
+    # decrypt_text(encrypted_texts[i], cipher_keys)
